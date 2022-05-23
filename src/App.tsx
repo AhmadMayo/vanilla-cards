5f292a5ae56547a9d948d53e2fd85d67f29cb67c
Ahmed Mayo <ahmedsabdelaziz@gmail.com>
2022-05-24 00:54:32 +0200

    implement fetching logic and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import type Post from "./Post";
+import PostCard from "./PostCard";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const [posts, setPosts] = useState<Post[]>([]);
 
-  return <div>Home</div>;
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(
+      "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json ",
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        if (response.status < 200 || response.status > 299) {
+          throw new Error("response error");
+        }
+
+        return response.json();
+      })
+      .then((result: Post[]) => setPosts(result))
+      .catch((error: Error) => setError(error))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div className="l-application">
+      <main className="l-main">
+        <div className="row">
+          {isLoading && (
+            <div className="u-align--center">
+              <i className="p-icon--spinner u-animation--spin"></i>
+            </div>
+          )}
+          {error != null && (
+            <div className="p-notification--negative">
+              <div className="p-notification__content">
+                <h2 className="p-notification__title">Error</h2>
+                <p className="p-notification__message">
+                  Error while fetching data.
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
